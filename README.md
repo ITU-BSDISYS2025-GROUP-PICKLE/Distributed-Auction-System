6af9da629aa46939cf774c647e1ac6a79c76375c
oshe <oshe@itu.dk>
2025-11-13 14:53:32 +0100

    Simple auction server start-up
    Co-Authored-By: Casper <225747490+shtaroo@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+## How to run
+%% WORK IN PROGRESS %%
+
 ## Introduction
 You must implement a **distributed auction system** using replication: a distributed component which handles auctions, and provides operations for bidding and querying the state of an auction. The component must faithfully implement the semantics of the system described below, and must at least be resilient to one (1) crash failure.
 
--- auction/auction.go
+++ auction/auction.go
@@ -1,14 +1,48 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net"
 	"sync"
+
+	"google.golang.org/grpc"
+
+	pb "module/proto"
 )
 
 type Auction struct {
-	mu sync.Mutex
+	pb.UnimplementedAuctionServer
+
+	mu          sync.Mutex
+	address     string
+	highest_bid pb.Bid
+}
+
+func (a *Auction) StartServer() {
+	// Create listener
+	lis, err := net.Listen("tcp", a.address)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	// Create and register server
+	server := grpc.NewServer()
+	pb.RegisterAuctionServer(server, a)
+
+	// Log for transparency
+	log.Printf("Auction now listening on %s", a.address)
+
+	// Serve
+	if err := server.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
 
 func main() {
-	fmt.Println("Auction")
+	auction := Auction{
+		address:     "localhost:8080",
+		highest_bid: pb.Bid{ClientId: -1, BidAmount: 0},
+	}
+
+	auction.StartServer()
 }
