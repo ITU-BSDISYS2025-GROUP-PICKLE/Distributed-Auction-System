03c9170f5b226376094224fde219b892cb11dbc4
oshe <oshe@itu.dk>
2025-11-21 16:52:51 +0100

    RPC function implementations
    Co-Authored-By: Casper <225747490+shtaroo@users.noreply.github.com>

--- node/node.go
+++ node/node.go
@@ -45,6 +45,8 @@ func (n *AuctionNode) StartServer() {
 	// Log for transparency
 	log.Printf("AuctionNode server now listening on %s", n.addr)
 
+	// TODO: Auction timer logic might be started here
+
 	// Serve
 	if err := server.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
@@ -53,19 +55,55 @@ func (n *AuctionNode) StartServer() {
 
 // RPC function
 func (n *AuctionNode) TryBid(_ context.Context, proposed_bid *pb.Bid) (*pb.Acknowledgement, error) {
-	// Needs implementation
-	return nil, nil
+	n.mu.Lock()
+	defer n.mu.Unlock()
+
+	// If the auction isn't alive, return an 'Exception'-acknowledgement
+	if !n.is_auction_live {
+		return &pb.Acknowledgement{AckType: pb.Acknowledgement_EXCEPTION}, nil
+	}
+
+	// If the auction is alive, and the proposed bid is higher than the current max,
+	// overwrite and return a 'Success'-acknowledgement
+	if proposed_bid.BidAmount > n.highest_bid_amount {
+		n.highest_bid_amount = proposed_bid.BidAmount
+		n.highest_bidder_id = proposed_bid.BiddingClientId
+		return &pb.Acknowledgement{AckType: pb.Acknowledgement_SUCCESS}, nil
+	}
+
+	// If the auction is alive, but the proposed bid is lower than the current max,
+	// simply return a 'Fail'-acknowledgement
+	return &pb.Acknowledgement{AckType: pb.Acknowledgement_FAIL}, nil
 }
 
 // RPC function
 func (n *AuctionNode) TryResult(_ context.Context, _ *pb.Empty) (*pb.Outcome, error) {
-	// Needs implementation
-	return nil, nil
+	n.mu.Lock()
+	defer n.mu.Unlock()
+
+	// If the auction isn't alive, return a 'Result'-outcome including the highest bidder and bid
+	if !n.is_auction_live {
+		return &pb.Outcome{
+			OutcomeType: &pb.Outcome_Result{
+				Result: &pb.Result{
+					HighestBidderId: n.highest_bidder_id,
+					HighestBidFinal: n.highest_bid_amount,
+				},
+			},
+		}, nil
+	}
+
+	// If the auction is alive, return a 'Highest-bid-so-far'-outcome
+	return &pb.Outcome{
+		OutcomeType: &pb.Outcome_HighestBidSoFar{
+			HighestBidSoFar: n.highest_bid_amount,
+		},
+	}, nil
 }
 
 func main() {
 	if len(os.Args) < 2 {
-		// Even though we're really only looking for 1 argument, we expect 2 or more because
+		// Even though we're really only looking for 1 argument, we expect 2 (or more) because
 		// the first (0th) argument always directs to the .exe of the .go file that's run
 		println("Usage: go run node.go <port_number>")
 		return
